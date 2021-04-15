8d8143a8437d526f68d0a87128d75bb313b17445
Grzegorz Wilczyński <grzesiek22081999w@gmail.com>
2021-04-15 17:39:57 +0200

    logowanie

---
+++ database/migrations/2021_04_14_000000_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',45);
+            $table->string('surname',45);
+            $table->string('nick',16);
+            $table->string('email',45)->unique();
+            $table->string('password',255);
+            $table->enum('status',['zablokowany','aktywny']);
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}

e2de3043739702a165d9e6027763ddb3793394c6
rzywczak <73279676+rzywczak@users.noreply.github.com>
2021-05-04 13:40:00 +0200

    Api do logowania

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -23,10 +23,29 @@ class ApiController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function logowanie()
+     public function logowanie()
     {
+        $data = array();
         header('Content-Type: application/json');
-        $u=DB::select("select * from users where email='" . $_GET['email'] . "'");
-        echo json_encode($u);
+        $password = urldecode($_GET["password"]);
+        $u = DB::select("select * from users where email='" . $_GET['email'] . "'");
+        if(count($u)>0){
+            if(password_verify($password,$u[0]->password)){
+                $data["status"]="success";
+                $data["message"]="";
+                $data["data"]=$u[0];
+            }
+            else{
+                $data["status"]="failed";
+                $data["message"]="nie udało sie zalogować";
+                $data["data"]=NULL;
+            }
+        }
+        else{
+            $data["status"]="failed";
+            $data["message"]="nie udało sie zalogować";
+            $data["data"]=NULL;
+        }
+        echo json_encode($data);
     }
 }

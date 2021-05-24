32fb8d0e0d2b2fba64a05b31e871e59c69378794
rzywczak <73279676+rzywczak@users.noreply.github.com>
2021-05-24 15:31:32 +0200

    API poprawka

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BanList;
+use App\Models\Files;
 use App\Models\FriendList;
 use DB;
 use Illuminate\Support\Facades\Auth;
-use App\Models\Message;
-use App\Models\Files;
-use App\Models\BanList;
 
 class ApiController extends Controller
 {
@@ -26,84 +25,83 @@ class ApiController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-     public function logowanie()
+    public function logowanie()
     {
         $data = array();
         header('Content-Type: application/json');
         $password = urldecode($_GET["password"]);
         $u = DB::select("select * from users where email='" . $_GET['email'] . "'");
-        if(count($u)>0){
-            if(password_verify($password,$u[0]->password)){
-                $data["status"]="success";
-                $data["message"]="";
-                $data["data"]=$u[0];
+        if (count($u) > 0) {
+            if (password_verify($password, $u[0]->password)) {
+                $data["status"] = "success";
+                $data["message"] = "";
+                $data["data"] = $u[0];
+                $data["data"]->avatar = "http://projektkt.pl/uploads/avatars/" . $data["data"]->avatar;
+            } else {
+                $data["status"] = "failed";
+                $data["message"] = "nie udało sie zalogować";
+                $data["data"] = null;
             }
-            else{
-                $data["status"]="failed";
-                $data["message"]="nie udało sie zalogować";
-                $data["data"]=NULL;
-            }
-        }
-        else{
-            $data["status"]="failed";
-            $data["message"]="nie udało sie zalogować";
-            $data["data"]=NULL;
+        } else {
+            $data["status"] = "failed";
+            $data["message"] = "nie udało sie zalogować";
+            $data["data"] = null;
         }
         echo json_encode($data);
     }
-    public function changeAvatar(){
-            $check = getimagesize($_FILES["file"]["tmp_name"]);
-            if (false !== $check) {
-                $roz=explode('/',$_FILES['file']['type']);
-                $nazwa=uniqid();
-                Files::create([
-                    'nazwa' => $nazwa,
-                    'rozszerzenie' => $roz[1],
-                    'przeznaczenie' => 'avatar',
-                    'uzytkownik_id'=>Auth::id()
-                ]);
-                move_uploaded_file($_FILES['file']['tmp_name'], __DIR__.'/../../../public/uploads/avatars/' . $nazwa.'.'.$roz[1]);
-                DB::update('UPDATE users SET avatar="'.$nazwa.'.'.$roz[1].'" WHERE id='.Auth::id());
+    public function changeAvatar()
+    {
+        $check = getimagesize($_FILES["file"]["tmp_name"]);
+        if (false !== $check) {
+            $roz = explode('/', $_FILES['file']['type']);
+            $nazwa = uniqid();
+            Files::create([
+                'nazwa' => $nazwa,
+                'rozszerzenie' => $roz[1],
+                'przeznaczenie' => 'avatar',
+                'uzytkownik_id' => Auth::id(),
+            ]);
+            move_uploaded_file($_FILES['file']['tmp_name'], __DIR__ . '/../../../public/uploads/avatars/' . $nazwa . '.' . $roz[1]);
+            DB::update('UPDATE users SET avatar="' . $nazwa . '.' . $roz[1] . '" WHERE id=' . Auth::id());
 
-                echo json_encode($nazwa.'.'.$roz[1]);
+            echo json_encode($nazwa . '.' . $roz[1]);
 
-            }else{
-                echo json_encode(0);
+        } else {
+            echo json_encode(0);
+        }
+    }
+
+    public function friend_list()
+    {
+        header('Content-Type: application/json');
+        $data = array();
+        if (isset($_GET['id'])) {
+            $friend_list = DB::select("select * from friend_list where (user_id=" . $_GET['id'] . " OR friend_id=" . $_GET['id'] . ") AND accepted=1");
+            $friend_list_arr = [];
+            foreach ($friend_list as $v) {
+                if ($_GET['id'] == $v->friend_id) {
+                    $w = DB::select("select id,name,surname,nick,email,avatar from users where id=" . $v->user_id);
+                } else {
+                    $w = DB::select("select id,name,surname,nick,email,avatar from users where id=" . $v->friend_id);
+                }
+                $w[0]->avatar = "http://projektkt.pl/uploads/avatars/" . $w[0]->avatar;
+
+                $friend_list_arr[] = $w[0];
             }
-    }
-    
-     public function friend_list()
-   {
-       header('Content-Type: application/json');
-       $data = array();
-       if(isset($_GET['id'])){
-       $friend_list = DB::select("select * from friend_list where (user_id=" . $_GET['id'] . " OR friend_id=" . $_GET['id'] . ") AND accepted=1");
-       $friend_list_arr = [];
-       foreach ($friend_list as $v) {
-           if ($_GET['id'] == $v->friend_id) {
-               $w = DB::select("select name,surname,nick,email,avatar from users where id=" . $v->user_id);
-           } else {
-               $w = DB::select("select name,surname,nick,email,avatar from users where id=" . $v->friend_id);
-           }
-            $w[0]->avatar="http://projektkt.pl/uploads/avatars/".$w[0]->avatar;
 
-           $friend_list_arr[] = $w[0];
-       }
+            $data["status"] = "success";
+            $data["message"] = "";
+            $data["data"] = $friend_list_arr;
 
-       $data["status"]="success";
-       $data["message"]="";
-       $data["data"]=$friend_list_arr;
+        } else {
 
-    }
-    else{
+            $data["status"] = "failed";
+            $data["message"] = "brak id";
+            $data["data"] = [];
 
-        $data["status"]="failed";
-        $data["message"]="brak id";
-        $data["data"]=[];
+        }
+        echo json_encode($data);
 
-    }
-       echo json_encode($data);
-       
     }
 
     public function wysylanie_zaproszenia()
@@ -177,6 +175,7 @@ class ApiController extends Controller
         if (isset($_GET['user_id'])) {
             if (isset($_GET['friend_id'])) {
                 DB::delete("DELETE FROM friend_list WHERE (user_id=" . $_GET['user_id'] . " AND friend_id=" . $_GET['friend_id'] . ") OR (user_id=" . $_GET['friend_id'] . " AND friend_id=" . $_GET['user_id'] . ")");
+                $data["status"] = "success";
             } else {
                 $data["status"] = "failed";
                 $data["message"] = "Nie podano friend_id ";
@@ -234,17 +233,22 @@ class ApiController extends Controller
     {
         $data = array();
         header('Content-Type: application/json');
-        if (isset($_GET['id'])) {
-            $u = DB::select("select * from friend_list where user_id=" . $_GET['id'] . " AND accepted=0");
-            $waiting_arr = [];
-            foreach ($waiting as $v) {
-                $w = DB::select("select * from users where id=" . $v->friend_id);
-                $w[0]->avatar = "http://projektkt.pl/uploads/avatars/" . $w[0]->avatar;
-                $waiting_arr[] = $w[0];
+        if (isset($_GET['user_id'])) {
+            $u = DB::select("select * from friend_list where user_id=" . $_GET['user_id'] . " AND accepted=0");
+            if (!empty($u)) {
+                $waiting_arr = [];
+                foreach ($u as $v) {
+                    $w = DB::select("select * from users where id=" . $v->friend_id);
+                    $w[0]->avatar = "http://projektkt.pl/uploads/avatars/" . $w[0]->avatar;
+                    $waiting_arr[] = $w[0];
+                }
+
+                $data["status"] = "success";
+                $data["data"] = $waiting_arr;
+            } else {
+                $data["status"] = "success";
+                $data["message"] = "Brak wyslanych zaproszen";
             }
-            
-            $data["status"] = "success";
-            $data["data"] = $waiting_arr;
         } else {
             $data["status"] = "failed";
             $data["message"] = "Nie podano id";
@@ -253,6 +257,7 @@ class ApiController extends Controller
 
         echo json_encode($data);
     }
+
     public function odebrane_zaproszenia()
     {
         header('Content-Type: application/json');
@@ -331,4 +336,5 @@ class ApiController extends Controller
         }
         echo json_encode($data);
     }
+
 }

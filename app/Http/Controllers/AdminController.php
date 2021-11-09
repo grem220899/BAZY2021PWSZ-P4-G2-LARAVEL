604230c85123d9ab354b40d31d399288b3120194
Grzegorz Wilczyński <grzesiek22081999w@gmail.com>
2021-11-09 10:51:46 +0100

    Admin poprawka

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,7 +11,15 @@ use App\Models\Zamienniki;
 
 class AdminController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth', 'verified']);
+    }
     public function index(){
+        if(Auth::user()->status!="admin"){
+            header("Location: /");
+            exit();
+        }
         $data=[];
         return view('admin.panel', $data);
     }
